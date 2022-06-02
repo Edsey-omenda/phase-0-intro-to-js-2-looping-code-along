3e194f6dec8aedc275d8246c3a824e38881aa0ba
Edsey omenda <omendajames0317@gmail.com>
2022-06-02 19:50:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+let cards =["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    let newCards = [];
+    for (let i = 0; i < cards.length; i++) {
+      newCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+
+    return newCards;
+  }
+
+
+  function countDown() {
+    let tallyDown = 10, n = 0;
+   while (tallyDown >= n) 
+  {
+    console.log(tallyDown--);
+  }
+  };
+  
+
+
+
+
+
+
